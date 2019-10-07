7b4e7586f6c27cb9825c773758393203f8b2faf6
Julia Aasmaa <my_e_10@hotmail.com>
2019-10-07 09:22:26 +0300

    GitHub Finder Ready

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
-loadEventListeners();
+//Init Github Class
+const github = new Github;
+//Init UI Class
+const ui = new UI;
+
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', (event) => {
+    //get the input text from the searchUser input field
+    const userText = event.target.value;
+    if(userText !== '') {
+        //make an http call to GitHub API
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === 'Not Found') {
+                //Show alert if user was not found
+                ui.showAlert('User Not Found', 'alert alert-danger');
+            } else {
+                //Show user profile and repos
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+            console.log(data);
+        })
+    }else {
+            ui.clearProfile();
+        }
+})
+
+
+
+
+
+
+
+/*loadEventListeners();
 
 function loadEventListeners() {
     document.addEventListener('DOMContentLoaded', getObject);
@@ -11,4 +46,4 @@ function getObject() {
     .then(data=> {
         console.log(data);
     });
-}
+}*/
--- ui.js
+++ ui.js
@@ -55,5 +55,38 @@ class UI {
             document.getElementById('repos').innerHTML = output;
         });
     }
+
+    showAlert(message, className) {
+        this.clearAlert();
+
+        //create alert div
+        const div = document.createElement('div');
+        //add class name
+        div.className = className;
+        //add text
+        div.appendChild(document.createTextNode(message));
+        //instert the div into its parent
+        const container = document.querySelector('.searchContainer');
+        const search = document.querySelector('.search');
+
+        container.insertBefore(div, search);
+
+        setTimeout(()=> {
+            this.clearAlert();
+        }, 3000);
+    }
+
+
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+        if(currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    clearProfile() {
+        this.profile.innerHTML = '';
+    }
+
 }
 
